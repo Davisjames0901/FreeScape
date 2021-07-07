0f9291efff3b3d124879fbb71147aca1991bd32b
Blake Harrington <blake.harrington14@gmail.com>
2021-07-07 14:24:15 -0400

    Updated Math.NearEquals

--- FreeScape.Engine/Utilities/Maths.cs
+++ FreeScape.Engine/Utilities/Maths.cs
@@ -22,34 +22,23 @@ namespace FreeScape.Engine.Utilities
             var y = Lerp(a.Y, b.Y, by);
             return new Vector2f(x, y);
         }
+        public static bool NearEquals(float a, float b, float maxEpsilon)
+        {
+            if (Math.Abs(b - a) < maxEpsilon)
+            {
+                return true;
+            }
+
+            return false;
+        }
         public static bool NearEquals(Vector2f a, Vector2f b, float maxEpsilon)
         {
-            if (Math.Abs(b.X - a.X) < maxEpsilon && Math.Abs(b.Y - a.Y) < maxEpsilon)
+            if (NearEquals(a.X, b.X, maxEpsilon) && NearEquals(a.Y, b.Y, maxEpsilon))
             {
                 return true;
             }
 
             return false;
-        }
-        public static bool NearEquals(Vector3f a, Vector3f b, float maxEpsilon)
-        {
-            if (Math.Abs(b.X - a.X) < maxEpsilon && Math.Abs(b.Y - a.Y) < maxEpsilon && Math.Abs(b.Z - a.Z) < maxEpsilon)
-            {
-                return true;
-            }
-
-            return false;
-        }
-        public static Vector3f Lerp(Vector3f a, Vector3f b, float by, float maxEpsilon)
-        {
-            if (NearEquals(b, a, maxEpsilon))
-            {
-                return b;
-            }
-            var x = Lerp(a.X, b.X, by);
-            var y = Lerp(a.Y, b.Y, by);
-            var z = Lerp(a.Z, b.Z, by);
-            return new Vector3f(x, y, z);
         }
 
         public static Vector2f Vector2ITo2F(Vector2i vec)
