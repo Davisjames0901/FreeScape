7877e64630fbb7669415e62b9c5cad539ad2dff2
digman <diggly123456@gmail.com>
2021-08-10 22:15:36 -0400

    Updated tests with some custom asserts

---
+++ FreeScape.Engine.Tests/CustomAsserts.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace FreeScape.Engine.Tests
+{
+    public static class CustomAsserts
+    {
+        public static void AssertInBounds(IEnumerable<double> source, double average, double maximumDelta)
+        {
+            var min = source.Min();
+            var max = source.Max();
+            Assert.Greater(min, average - maximumDelta, $"Found a value ({min}) which was lower than the permitted lower bound of ${average - maximumDelta}");
+            Assert.Less(max, average + maximumDelta, $"Found a value ({max}) which was higher than the permitted upper bound of ${average + maximumDelta}");
+        }
+
+        public static void AverageIsEqual(IEnumerable<double> source, double expectedValue, double maximumDelta = 0)
+        {
+            Assert.AreEqual(expectedValue, source.Average(), maximumDelta);
+        }
+    }
+}
--- FreeScape.Engine.Tests/ProviderTests/FrameTimeTests.cs
+++ FreeScape.Engine.Tests/ProviderTests/FrameTimeTests.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using FreeScape.Engine.Providers;
 using NUnit.Framework;
 
@@ -7,8 +6,11 @@ namespace FreeScape.Engine.Tests.ProviderTests
 {
     public class FrameTimeTests
     {
+        private const int NUMER_OF_RUNS = 20;
+        private const double ACCEPTABLE_ACCURACY = 0.1;
+        private const double MAXIMUM_DELTA = 0.4;
         private FrameTimeProvider _frameTime;
-        
+
         [SetUp]
         public void Setup()
         {
@@ -19,28 +21,30 @@ namespace FreeScape.Engine.Tests.ProviderTests
         public void AccuratelyMeasures1Millisecond()
         {
             var times = new List<double>();
-            for (var i = 0; i < 20; i++)
+            for (var i = 0; i < NUMER_OF_RUNS; i++)
             {
                 _frameTime.Tick();
                 AccurateWaiter.WaitMs(1);
                 _frameTime.Tick();
                 times.Add(_frameTime.DeltaTimeMilliSeconds);
             }
-            Assert.AreEqual(1d, times.Average(), 0.01);
+            CustomAsserts.AverageIsEqual(times, 1, ACCEPTABLE_ACCURACY);
+            CustomAsserts.AssertInBounds(times, 1, MAXIMUM_DELTA);
         }
 
         [Test]
         public void AccuratelyMeasures1Second()
         {
             var times = new List<double>();
-            for (var i = 0; i < 20; i++)
+            for (var i = 0; i < NUMER_OF_RUNS; i++)
             {
                 _frameTime.Tick();
                 AccurateWaiter.WaitMs(1000);
                 _frameTime.Tick();
                 times.Add(_frameTime.DeltaTimeMilliSeconds);
             }
-            Assert.AreEqual(1, times.Average(), 0.01);
+            CustomAsserts.AverageIsEqual(times, 1, ACCEPTABLE_ACCURACY);
+            CustomAsserts.AssertInBounds(times, 1, MAXIMUM_DELTA);
         }
     }
 }
